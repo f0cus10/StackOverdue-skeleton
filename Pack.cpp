cb8f3f279b6fb977eb9d7ef8869f24f722ea6a40
Iftikhar Khan <iakhan4425@gmail.com>
2017-12-14 09:15:01 -0500

    Added Pack implementation

--- Pack.cpp
+++ Pack.cpp
@@ -7,3 +7,29 @@
 //
 
 #include "Pack.hpp"
+#include "Account.hpp"
+#include <vector>
+//Default Constructor
+//Overloaded Destructor
+Pack::~Pack(){
+  for (auto iter = accounts.begin(); iter != accounts.end(); ++iter) {
+    delete iter->second;
+  }
+  accounts.clear();
+}
+//Given an account, add it to the collection
+void Pack::addAccount(Account* newAccount){
+  accounts[newAccount->getID()] = newAccount;
+}
+//Return a vector containing all the accounts for the sorting algorithms
+vector<const Account*> Pack::getAccounts() const {
+  vector<const Account*> tmp (accounts.size());
+  for(auto iter = accounts.begin(); iter != accounts.end(); ++iter){
+    tmp.push_back(iter->second);
+  }
+  return tmp;
+}
+//Return a const-pointer to the account, given accountID
+const Account* Pack::getAccount(unsigned int accountID){
+  return accounts[accountID];
+}
